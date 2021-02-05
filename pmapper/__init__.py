a59d14b6cf2ce670303c45c61f401535c9d8a5e5
Brandon Dube <brandon.dube@jpl.nasa.gov>
2021-02-05 14:33:03 -0800

    + multi-frame PMAP

--- pmapper/__init__.py
+++ pmapper/__init__.py
@@ -36,11 +36,11 @@ class PMAP:
             psf corresponding to img, ndarray of shape (n, m)
         fhat : `numpy.ndarray`
             initial object estimate, ndarray of shape (n, m)
+            if None, taken to be the img
 
         """
         self.img = img
         self.psf = psf
-        self.fHat = fHat
 
         otf = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(psf)))
         center = tuple(s // 2 for s in otf.shape)
@@ -50,7 +50,9 @@ class PMAP:
 
         if fHat is None:
             fHat = img
+
         self.fHat = fHat
+        self.iter = 0
 
     def step(self):
         """Iterate the algorithm one step.
@@ -69,4 +71,82 @@ class PMAP:
         R = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(kernel)))
         grad = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(R * self.otfconj))).real
         self.fHat = self.fHat * np.exp(grad)
+        self.iter += 1
+        return self.fHat
+
+
+class MFPMAP:
+    """Multi-Frame PMAP algorithm.  Suitable for panchromatic restoration.
+
+    Implements Ref [1], Eq. 2.26.
+    """
+
+    def __init__(self, imgs, psfs, fHat=None):
+        """Initialize a new PMAP problem.
+
+        Parameters
+        ----------
+        imgs : `numpy.ndarray`
+            images from the camera, sequence of ndarray of shape (n, m).
+            The images must be fully co-registered before input to the algorithm.
+            A (k, n, m) shaped array iterates correctly, as does a list or other
+            iterable of (n, m) arrays
+        psfs : `numpy.ndarray`
+            psfs corresponding to imgs, sequence of ndarray of shape (n, m)
+        fhat : `numpy.ndarray`
+            initial object estimate, ndarray of shape (n, m)
+            if None, taken to be the first img
+
+        Notes
+        -----
+        This implementation is optimized for performance on hardware with a large
+        amount of memory.  The OTFs can be computed during each step to use less
+        memory overall, in exchange for slower iterations.
+
+        """
+        self.imgs = imgs
+        self.psfs = psfs
+
+        otfs = [np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(psf))) for psf in psfs]
+        center = tuple(s // 2 for s in otfs[0].shape)
+        for otf in otfs:
+            otf /= otf[center]  # definition of OTF, normalize by DC
+
+        self.otfs = otfs
+        self.otfsconj = [np.conj(otf) for otf in otfs]
+
+        if fHat is None:
+            fHat = imgs[0]
+
+        self.fHat = fHat
+        self.iter = 0
+
+    def step(self):
+        """Iterate the algorithm one step.
+
+        Because this implementation cycles through the images, the steps can be
+        thought of as mini-batches.  Intuitively, you may wish to make len(imgs)
+        steps at a time.
+
+        Returns
+        -------
+        fhat : `numpy.ndarray`
+            updated object estimate, of shape (n, m)
+
+        """
+        i = self.iter % len(self.otfs)
+
+        otf = self.otfs[i]
+        img = self.imgs[i]
+        otfconj = self.otfsconj[i]
+
+        Fhat = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(self.fHat)))
+        denom = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(Fhat * otf))).real
+
+        # # kernel is the expression { g/(f_n conv h) - 1 } from 2.16, J. J. Green's thesis
+        kernel = (img / denom) - 1
+        R = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(kernel)))
+        grad = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(R * otfconj))).real
+        self.fHat = self.fHat * np.exp(grad)
+        self.iter += 1
         return self.fHat
