e41af90b0d25f4a01d738bd9b8808c36c03a3a81
Brandon Dube <brandon.dube@jpl.nasa.gov>
2022-04-01 08:44:18 -0700

    fix broken imports in pkg init

--- pmapper/__init__.py
+++ pmapper/__init__.py
@@ -1,20 +1,8 @@
 """pmapper: a super-resolution toolkit."""
 
-from .pmap import (
-    PMAP,
-    MFPMAP,
-    BayerPMAP,
-    BayerMFPMAP
-)
+from .pmap import *
 
 from .rl import (
     RichardsonLucy
 )
 
-__all__ = [
-    'PMAP',
-    'MFPMAP',
-    'BayerPMAP',
-    'BayerMFPMAP',
-    'RichardsonLucy'
-]
